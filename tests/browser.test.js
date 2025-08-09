f850555b9b7cfe74e55bad0de99de3154ba5777b
ao9792 <144122513+ao9792@users.noreply.github.com>
2025-08-09 13:37:15 +0200

    Failar tester för tom stack (unit + e2e)

--- tests/browser.test.js
+++ tests/browser.test.js
@@ -1,36 +1,51 @@
-const { Builder, By, until } = require('selenium-webdriver');
+const { Builder, By } = require('selenium-webdriver');
 require('geckodriver');
 
-// Här anger vi var testfilen ska hämtas. De konstiga replaceAll-funktionerna ersätter
-// mellanslag med URL-säkra '%20' och backslash (\) på Windows med slash (/).
-const fileUnderTest = 'file://' + __dirname.replaceAll(/ /g, '%20').replaceAll(/\\/g, '/') + '/../dist/index.html';
+const fileUnderTest =
+  'file://' +
+  __dirname.replaceAll(/ /g, '%20').replaceAll(/\\/g, '/') +
+  '/../dist/index.html';
+
 const defaultTimeout = 10000;
 let driver;
-jest.setTimeout(1000 * 60 * 5); // 5 minuter
+jest.setTimeout(1000 * 60 * 5);
 
-// Det här körs innan vi kör testerna för att säkerställa att Firefox är igång
 beforeAll(async () => {
-console.log(fileUnderTest);
-    driver = await new Builder().forBrowser('firefox').build();
-    await driver.get(fileUnderTest);
+  driver = await new Builder().forBrowser('firefox').build();
+  await driver.get(fileUnderTest);
 });
 
-// Allra sist avslutar vi Firefox igen
-afterAll(async() => {
-    await driver.quit();
+afterAll(async () => {
+  await driver.quit();
 }, defaultTimeout);
 
 test('The stack should be empty in the beginning', async () => {
-    let stack = await driver.findElement(By.id('top_of_stack')).getText();
-    expect(stack).toEqual("n/a");
+  const text = await driver.findElement(By.id('top_of_stack')).getText();
+  expect(text).toEqual('n/a');
 });
 
 describe('Clicking "Pusha till stacken"', () => {
-    it('should open a prompt box', async () => {
-        let push = await driver.findElement(By.id('push'));
-        await push.click();
-        let alert = await driver.switchTo().alert();
-        await alert.sendKeys("Bananer");
-        await alert.accept();
-    });
+  it('should open a prompt box and add the value to the stack', async () => {
+    const push = await driver.findElement(By.id('push'));
+    await push.click();
+    const alert = await driver.switchTo().alert();
+    await alert.sendKeys('Bananer');
+    await alert.accept();
+
+    const stackTop = await driver.findElement(By.id('top_of_stack')).getText();
+    expect(stackTop).toEqual('Bananer');
+  });
+});
+
+test('Poppa på tom stack visar felmeddelande (ska FAILA nu)', async () => {
+  await driver.navigate().refresh();
+  const popBtn = await driver.findElement(By.id('pop'));
+  await popBtn.click();
+
+  const alert = await driver.switchTo().alert();
+  const text = await alert.getText();
+
+  expect(text).toBe('Kan inte poppa från tom stack'); // -> FAIL nu
+
+  await alert.accept();
 });
